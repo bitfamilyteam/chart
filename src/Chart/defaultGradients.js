2a7d910f46ea52d409718243716ece132fde5950
Dmitriy Bragin <jekyll@live.ru>
2018-12-03 16:11:42 +0700

    Removed excess background, compressed BG image (#2)

--- src/Chart/defaultGradients.js
+++ src/Chart/defaultGradients.js
@@ -21,46 +21,6 @@ export default {
       },
     ],
   },
-  backgroundGrow: {
-    coords: {
-      x1: `${108.216 / 375 * 100}%`,
-      y1: `${462.846 / 812 * 100}%`,
-      x2: `${581.544 / 375 * 100}%`,
-      y2: `${135.35 / 812 * 100}%`,
-    },
-    stops: [
-      {
-        offset: '0%',
-        color: '#5e71f7',
-        opacity: 0.17,
-      },
-      {
-        offset: '100%',
-        color: '#5b69ff',
-        opacity: 0,
-      },
-    ],
-  },
-  backgroundDecline: {
-    coords: {
-      x1: `${(591.262 - 454) / 375 * 100}%`,
-      y1: `${498.132 / 812 * 100}%`,
-      x2: `${(1134.256 - 454) / 375 * 100}%`,
-      y2: `${323.792 / 812 * 100}%`,
-    },
-    stops: [
-      {
-        offset: '0%',
-        color: '#f7b15e',
-        opacity: 0.17,
-      },
-      {
-        offset: '100%',
-        color: '#f96083',
-        opacity: 1,
-      },
-    ],
-  },
   areaChartFill: {
     coords: {
       x1: '0%',
--- src/Chart/index.js
+++ src/Chart/index.js
@@ -5,20 +5,13 @@ import moment from 'moment';
 import React from 'react';
 import type { Node } from 'react';
 import * as shape from 'd3-shape';
-import {
-  View, StyleSheet, PanResponder,
-} from 'react-native';
+import { View, StyleSheet, PanResponder } from 'react-native';
 import AnimatedChart from './AnimatedChart';
-import type {
-  PressEvent,
-  LayoutEvent,
-  Point,
-} from './types';
+import type { PressEvent, LayoutEvent, Point } from './types';
 import { handleSingleDataPoint } from './helpers';
 import Gradient from './Gradient';
 import type { GradientOptions } from './Gradient';
 import RateSection from './RateSection';
-import Background from './Background';
 import TooltipText from './Tooltip/TooltipText';
 import defaultGradients from './defaultGradients';
 
@@ -26,15 +19,15 @@ type ContentInset = {
   left: number,
   right: number,
   bottom: number,
-  top: number
-}
+  top: number,
+};
 
 type ChartProps = {
   data: Array<Point>,
   strokeWidth: number,
   period?: string,
   currency?: string,
-  gradientOptions?: {[key: string] : GradientOptions},
+  gradientOptions?: { [key: string]: GradientOptions },
   tooltip?: any,
   style?: StyleSheet.Styles,
   contentInset?: ContentInset,
@@ -43,10 +36,10 @@ type ChartProps = {
 
 type ChartState = {
   width: number,
-  height: number
+  height: number,
 };
 
-function getDefaultFontFamily() : string {
+function getDefaultFontFamily(): string {
   return 'System';
 }
 
@@ -61,15 +54,9 @@ function addInset(first: ContentInset, second?: ContentInset): ContentInset {
 
 const RATE_SECTION_HEIGHT = 60;
 
-function generateGradients(gradientOptions: {[key: string] : GradientOptions}): Array<Node> {
+function generateGradients(gradientOptions: { [key: string]: GradientOptions }): Array<Node> {
   const keys = R.keys(gradientOptions);
-  return R.map(key => (
-    <Gradient
-      key={`gradient_${key}`}
-      id={key}
-      options={gradientOptions[key]}
-    />
-  ), keys);
+  return R.map(key => <Gradient key={`gradient_${key}`} id={key} options={gradientOptions[key]} />, keys);
 }
 
 function calculateRightInset(period?: string, width: number, data: Array<Point>): number {
@@ -77,182 +64,163 @@ function calculateRightInset(period?: string, width: number, data: Array<Point>)
   if (period !== 'day' || data.length === 0) return 0;
 
   const currentTime = data[data.length - 1].x;
-  const startOfDayTime = moment(currentTime).startOf('day').valueOf();
-  const endOfDayTime = moment(startOfDayTime).add(1, 'day').valueOf();
+  const startOfDayTime = moment(currentTime)
+    .startOf('day')
+    .valueOf();
+  const endOfDayTime = moment(startOfDayTime)
+    .add(1, 'day')
+    .valueOf();
   const part = (endOfDayTime - currentTime) / (endOfDayTime - startOfDayTime);
 
   return 30 + (width - 30) * part;
 }
 class Chart extends React.PureComponent<ChartProps, ChartState> {
-    static defaultProps = {
-      strokeWidth: 2,
-    }
+  static defaultProps = {
+    strokeWidth: 2,
+  };
 
-    constructor(props: ChartProps) {
-      super(props);
-      this.state = {
-        width: 0,
-        height: 0,
-      };
+  constructor(props: ChartProps) {
+    super(props);
+    this.state = {
+      width: 0,
+      height: 0,
+    };
 
-      this.panResponder = PanResponder.create({
-        onStartShouldSetPanResponder: R.T,
-        onStartShouldSetPanResponderCapture: R.T,
-        onMoveShouldSetPanResponder: R.T,
-        onMoveShouldSetPanResponderCapture: R.T,
-        onPanResponderGrant: this.onStartTouch,
-        onPanResponderMove: this.onTouch,
-        onPanResponderRelease: this.onEndTouch,
-        onPanResponderTerminationRequest: R.T,
-        onPanResponderTerminate: R.T,
-      });
-    }
+    this.panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: R.T,
+      onStartShouldSetPanResponderCapture: R.T,
+      onMoveShouldSetPanResponder: R.T,
+      onMoveShouldSetPanResponderCapture: R.T,
+      onPanResponderGrant: this.onStartTouch,
+      onPanResponderMove: this.onTouch,
+      onPanResponderRelease: this.onEndTouch,
+      onPanResponderTerminationRequest: R.T,
+      onPanResponderTerminate: R.T,
+    });
+  }
 
-    panResponder: PanResponder;
-    tooltip: any;
-    tooltipText: any;
-    rateSection: any;
+  panResponder: PanResponder;
+  tooltip: any;
+  tooltipText: any;
+  rateSection: any;
 
-    onTooltipRef = (tooltip: any) => {
-      this.tooltip = tooltip;
-    }
+  onTooltipRef = (tooltip: any) => {
+    this.tooltip = tooltip;
+  };
 
-    onTooltipTextRef = (tooltipText: any) => {
-      this.tooltipText = tooltipText;
-    }
+  onTooltipTextRef = (tooltipText: any) => {
+    this.tooltipText = tooltipText;
+  };
 
-    onRateSectionRef = (rateSection: any) => {
-      this.rateSection = rateSection;
-    }
+  onRateSectionRef = (rateSection: any) => {
+    this.rateSection = rateSection;
+  };
 
-    onStartTouch = (event: PressEvent) => {
-      this.onTouch(event);
-    }
+  onStartTouch = (event: PressEvent) => {
+    this.onTouch(event);
+  };
 
-    onEndTouch = () => {
-      this.setPosition();
-    }
+  onEndTouch = () => {
+    this.setPosition();
+  };
 
-    onTouch = (event: PressEvent) => {
-      const { data } = this.props;
-      if (data.length < 2 || !this.tooltip) return;
-      if (this.props.period === 'live') return;
+  onTouch = (event: PressEvent) => {
+    const { data } = this.props;
+    if (data.length < 2 || !this.tooltip) return;
+    if (this.props.period === 'live') return;
 
-      const { width } = this.state;
-      const { locationX } = event.nativeEvent;
-      const { left, right } = this.getContentInset();
-      const part = Math.min(1, (locationX - left) / (width - left - right));
+    const { width } = this.state;
+    const { locationX } = event.nativeEvent;
+    const { left, right } = this.getContentInset();
+    const part = Math.min(1, (locationX - left) / (width - left - right));
 
-      const min = data[0].x;
-      const max = data[data.length - 1].x;
-      const position = min + part * (max - min);
-      this.setPosition(position);
-    }
+    const min = data[0].x;
+    const max = data[data.length - 1].x;
+    const position = min + part * (max - min);
+    this.setPosition(position);
+  };
 
-    setPosition(position?: number) {
-      this.tooltip.setPosition(position);
-      this.tooltipText.setPosition(position);
-      this.rateSection.setPosition(position);
-    }
+  setPosition(position?: number) {
+    this.tooltip.setPosition(position);
+    this.tooltipText.setPosition(position);
+    this.rateSection.setPosition(position);
+  }
 
-    onLayout = (event: LayoutEvent): void => {
-      const { width, height } = event.nativeEvent.layout;
-      this.setState({ width, height });
-    }
+  onLayout = (event: LayoutEvent): void => {
+    const { width, height } = event.nativeEvent.layout;
+    this.setState({ width, height });
+  };
 
-    getContentInset(): ContentInset {
-      const { width, height } = this.state;
-      const { data, period } = this.props;
-      if (data.length === 1) {
-        return {
-          left: 0,
-          right: 0,
-          bottom: parseInt(height) / 2,
-          top: parseInt(height) / 2,
-        };
-      }
-
+  getContentInset(): ContentInset {
+    const { width, height } = this.state;
+    const { data, period } = this.props;
+    if (data.length === 1) {
       return {
         left: 0,
-        bottom: 40,
-        top: 100,
-        right: calculateRightInset(period, width, data),
+        right: 0,
+        bottom: parseInt(height) / 2,
+        top: parseInt(height) / 2,
       };
     }
 
-    render() {
-      const {
-        data, strokeWidth, tooltip, period, currency, fontFamily = getDefaultFontFamily(),
-      } = this.props;
-      if (!data.length) return <View />;
-      const { width, height } = this.state;
-      const newData = handleSingleDataPoint(data);
-      const gradientOptions = R.mergeDeepRight(defaultGradients, this.props.gradientOptions || {});
+    return {
+      left: 0,
+      bottom: 40,
+      top: 100,
+      right: calculateRightInset(period, width, data),
+    };
+  }
 
-      const isGrowing = newData[0].y <= newData[newData.length - 1].y;
+  render() {
+    const { data, strokeWidth, tooltip, period, currency, fontFamily = getDefaultFontFamily() } = this.props;
+    if (!data.length) return <View />;
+    const { width, height } = this.state;
+    const newData = handleSingleDataPoint(data);
+    const gradientOptions = R.mergeDeepRight(defaultGradients, this.props.gradientOptions || {});
 
-      return (
-        <View
-          style={this.props.style || { flex: 1 }}
-          onLayout={this.onLayout}
-          {...this.panResponder.panHandlers}
-        >
-          <Background
-            width={width}
-            height={height}
-            gradientOptions={
-              (isGrowing ? gradientOptions.backgroundGrow : gradientOptions.backgroundDecline)
-              || gradientOptions.background
-            }
-            id="background"
-          />
-          <View style={{ top: 130 }}>
-            <RateSection
-              ref={this.onRateSectionRef}
-              height={RATE_SECTION_HEIGHT}
-              data={data}
-              period={period}
-              fontFamily={fontFamily}
-            />
-          </View>
-          <View
-            style={{
-              position: 'absolute',
-              top: 210,
-            }}
-          >
-            <TooltipText
-              ref={this.onTooltipTextRef}
-              fontFamily="QuickFuse"
-              period={period}
-              width={width}
-              data={data}
-            />
-          </View>
-          <AnimatedChart
-            onTooltipRef={this.onTooltipRef}
-            style={{ flex: 1 }}
-            data={newData}
-            curve={shape.curveLinear}
-            contentInset={addInset(this.getContentInset(), this.props.contentInset)}
-            width={width}
-            height={height - RATE_SECTION_HEIGHT}
-            tooltip={tooltip}
+    return (
+      <View style={this.props.style || { flex: 1 }} onLayout={this.onLayout} {...this.panResponder.panHandlers}>
+        <View style={{ top: 130 }}>
+          <RateSection
+            ref={this.onRateSectionRef}
+            height={RATE_SECTION_HEIGHT}
+            data={data}
             period={period}
-            currency={currency}
             fontFamily={fontFamily}
-            svg={{
-              strokeWidth,
-              strokeLinejoin: 'round',
-              stroke: 'url(#areaChart)',
-              fill: 'url(#areaChartFill)',
-            }}
-          >
-            {generateGradients(R.pick(['areaChart', 'areaChartFill'], gradientOptions))}
-          </AnimatedChart>
+          />
         </View>
-      );
-    }
+        <View
+          style={{
+            position: 'absolute',
+            top: 210,
+          }}
+        >
+          <TooltipText ref={this.onTooltipTextRef} fontFamily="QuickFuse" period={period} width={width} data={data} />
+        </View>
+        <AnimatedChart
+          onTooltipRef={this.onTooltipRef}
+          style={{ flex: 1 }}
+          data={newData}
+          curve={shape.curveLinear}
+          contentInset={addInset(this.getContentInset(), this.props.contentInset)}
+          width={width}
+          height={height - RATE_SECTION_HEIGHT}
+          tooltip={tooltip}
+          period={period}
+          currency={currency}
+          fontFamily={fontFamily}
+          svg={{
+            strokeWidth,
+            strokeLinejoin: 'round',
+            stroke: 'url(#areaChart)',
+            fill: 'url(#areaChartFill)',
+          }}
+        >
+          {generateGradients(R.pick(['areaChart', 'areaChartFill'], gradientOptions))}
+        </AnimatedChart>
+      </View>
+    );
+  }
 }
 
 export default Chart;
